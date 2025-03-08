ac58b304c10a96ea26e2959c9d7c779e068e4dca
Oscar21122 <oscarzhaoxu@gmail.com>
2025-03-08 01:04:38 -0600

    Changed the connection to the database to an env variable

--- Backend/firebase/__init__.py
+++ Backend/firebase/__init__.py
@@ -1,23 +1,19 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
-from dotenv import load_dotenv
-import os
 
 # Inicializar Firebase
 # Obtener la ruta del archivo de credenciales
-firebase_credentials_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
 
-# Verificar si la ruta al archivo es válida
-if firebase_credentials_path is None:
-    print("Error: la variable de entorno FIREBASE_CREDENTIALS_PATH no está definida.")
+if firebase_credentials:
+    cred_dict = json.loads(firebase_credentials)  # Convertir el JSON en un diccionario
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred)
 else:
-    if os.path.exists(firebase_credentials_path):
-        print("¡Archivo de credenciales encontrado!")
-    else:
-        print(f"Error: el archivo de credenciales no existe en la ruta: {firebase_credentials_path}")
+    raise ValueError("No se encontraron credenciales de Firebase en las variables de entorno")
 
-cred = credentials.Certificate(firebase_credentials_path)  #
-firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 epics_ref = db.collection("Epics")
