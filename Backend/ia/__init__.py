cada0c2242a1f4fb90bf81ac4a8f7fc2a82e93b7
Abdiel <131422484+AbdielFritsche@users.noreply.github.com>
2025-03-08 23:35:05 -0600

    RAG creation for AI and methods for requirement generation

--- Backend/ia/__init__.py
+++ Backend/ia/__init__.py
@@ -1,16 +1,55 @@
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Standard library imports
 import os
 # Third-party imports
-from google import genai
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_chroma import Chroma
+from langchain.chains.retrieval import create_retrieval_chain 
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 # Local application imports
 
-load_dotenv(dotenv_path='../.env')
+#load_dotenv(dotenv_path='../.env')
 
 KEY = os.getenv("GEMINI_API_KEY")
 
-def intializeGeminiClient() -> genai:
-    GeminiClient = genai.Client(api_key=KEY)
+class ProjectAssistantAI:
+    def __init__(self, subdirectory, embedding_model = "models/embedding-001"):
+        pdf_directory = os.path.join(os.path.dirname(__file__), 'pdfs', subdirectory)
+        self.pdf_paths = [os.path.join(pdf_directory, f) for f in os.listdir(pdf_directory) if f.endswith('.pdf')]
+        self.embedding_model = embedding_model
+        self.vectorestore=None
+        self.llm = ChatGoogleGenerativeAI(
+            api_key=KEY,
+            model="gemini-1.5-pro",
+            temperature=0.2,
+            max_tokens=None,
+            timeout=None
+        )
+        self.load_and_vectorize_documents()
 
-    return GeminiClient
+    def load_and_vectorize_documents(self):
+        documents = []
+        for path in self.pdf_paths:
+            loader = PyPDFLoader(path)
+            data = loader.load()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000)
+            docs = text_splitter.split_documents(data)
+            documents.extend(docs)
+        self.vectorstore = Chroma.from_documents(
+            documents=documents,
+            embedding=GoogleGenerativeAIEmbeddings(google_api_key=KEY,model=self.embedding_model)
+        )
+
+    async def generate_content(self, query, preprompt):
+        prompt_template = ChatPromptTemplate.from_messages([
+            ("system", f"{preprompt} Utiliza la información siguiente para profundizar y enriquecer tu respuesta o como una base para construir lo que se te pide:" + "\n\n{context}"),
+            ("human", f"Pregunta: {query}")
+        ])
+        question_answer_chain = create_stuff_documents_chain(self.llm, prompt_template)
+        rag_chain = create_retrieval_chain(self.vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5}), question_answer_chain)
+        response = rag_chain.invoke({"input": query})
+        return response['answer']
--- Backend/routes/req_routes_ia.py
+++ Backend/routes/req_routes_ia.py
@@ -1,47 +1,73 @@
 # Standard library imports
-
+import asyncio 
+import os
 # Third-party imports
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 # Local application imports
-from ia import intializeGeminiClient
+from ia import ProjectAssistantAI as Assistant
+
 
 router = APIRouter()
-GeminiClient = intializeGeminiClient()
 
-
+RequirementsGenerativeAI = Assistant(subdirectory='requirements_pdfs')
 
 #PROMPTS BASE
-RequirementsPrompt = "Imagina que eres un SCRUM Master con 20 años de experiencia en metodologías Agile. " \
-"Tu tarea es generar requisitos funcionales y no funcionales detallados y específicos basados en " \
+FunctionalRequirementsPrompt = "Imagina que eres un SCRUM Master con 20 años de experiencia en metodologías Agile. " \
+"Tu tarea es generar requisitos funcionales detallados y específicos basados en " \
 "la descripción del proyecto que se te proporcionará. Debes ser conciso y evitar redundancias. " \
 "Responde únicamente cuando recibas una descripción clara y válida de un proyecto de software. " \
 "Si la descripción del proyecto es insuficiente para generar los requerimientos, pide detalles" \
 "específicos que falten. Por ejemplo, si necesitas más información sobre los usuarios finales" \
 "del sistema o los objetivos específicos del proyecto, indícalo claramente. Presenta los requerimientos" \
-" en una lista clara, dividiendo los funcionales y no funcionales. Para cada requerimiento," \
-" proporciona una breve explicación de su propósito y cómo contribuye al éxito del proyecto."
+" en una lista clara. Basate en el siguiente ejemplo: " \
+"1. Inicio de sesión de usuario: El sistema debe permitir a los usuarios iniciar sesión utilizando un nombre de usuario y contraseña válidos."\
+"2. Procesamiento de Negocios: El sistema debe procesar los pagos con tarjeta de crédito y proporcionar a los usuarios un recibo cuando las transacciones sean exitosas."
 
+NonFunctionalRequirementsPrompt = "Imagina que eres un SCRUM Master con 20 años de experiencia en metodologías Agile. " \
+"Tu tarea es generar requisitos no funcionales detallados y específicos basados en " \
+"la descripción del proyecto que se te proporcionará. Debes ser conciso y evitar redundancias. " \
+"Responde únicamente cuando recibas una descripción clara y válida de un proyecto de software. " \
+"Si la descripción del proyecto es insuficiente para generar los requerimientos, pide detalles" \
+"específicos que falten. Por ejemplo, si necesitas más información sobre los usuarios finales" \
+"del sistema o los objetivos específicos del proyecto, indícalo claramente. Presenta los requerimientos" \
+" en una lista clara. Basate en el siguiente ejemplo: " \
+"Velocidad de rendimiento:El sistema debe procesar las solicitudes de los usuarios en un plazo promedio de 2 segundos, incluso con mucho tráfico de usuarios."\
+"Disponibilidad del sistema:El sistema debe mantener un tiempo de actividad del 99.9 % para garantizar que los usuarios tengan acceso constante."
 
-
-"""
-Generar requerimientos
-"""
 
 class RequestBody(BaseModel):
     project_description: str
 
+#Generar requerimientos funcionales
+def generate_functional_requirements(project_description):
+
+    return RequirementsGenerativeAI.generate_content(
+        query=project_description,
+        preprompt=FunctionalRequirementsPrompt
+    )
+    
+#Generar requerimientos no funcionales
+def generate_non_functional_requirements(project_description):
+    return  RequirementsGenerativeAI.generate_content(
+        query=project_description,
+        preprompt=NonFunctionalRequirementsPrompt
+    )
+    
+
+"""
+Generar requerimientos funcionales 
+"""
 @router.post("/generate-requirements")
-async def generateRequirements(body: RequestBody):
+async def generate_requirements(body: RequestBody):
     project_description = body.project_description
     try:
-        full_prompt = RequirementsPrompt + " " + project_description
-        
-        response = GeminiClient.models.generate_content(
-            model = "gemini-2.0-flash",
-            contents={full_prompt}
-        )
-        return JSONResponse(content=response.text,status_code=200)
+        functional_response = await generate_functional_requirements(project_description),
+        non_functional_response = await generate_non_functional_requirements(project_description)
+        return JSONResponse(content={
+            "FR": functional_response,
+            "NFR": non_functional_response
+        }, status_code=200)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
